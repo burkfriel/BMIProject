abdc35e280afcbf37fc6a1a8b3e4d4bddbb62aaf
Burk Friel <burk.friel.2022@student.lhschools.org>
2018-09-14 14:18:25 -0400

    Finished It

--- src/BMIProject.java
+++ src/BMIProject.java
@@ -1,7 +1,35 @@
+import java.util.Scanner;
+
 public class BMIProject {
 
-    public static void main(String[]) args{
-        
+    public static void main(String[] args){
+        //Declare
+        int Inches;
+        int Pounds;
+        float Meters;
+        float Kilograms;
+        float Total;
+
+
+        Scanner keyboard;
+
+        //initializing
+
+        keyboard = new Scanner(System.in);
+
+
+
+        System.out.println("What is your height in inches.");
+        Inches = keyboard.nextInt();
+        Meters = Inches *(0.0254f);
+        System.out.println("What is your weight in pounds.");
+        Pounds = keyboard.nextInt();
+        Kilograms = Pounds *(0.453592f);
+        Total = Kilograms/Meters*Meters;
+        System.out.println("Your BMI is "+Total+".");
+
+
+
 
 
 
